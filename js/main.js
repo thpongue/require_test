879bd98fac44f6ff137be62fa5cc7fc9cc6bc548
Dominic McAdden <dominic.mcadden@gmail.com>
2015-01-21 16:19:59 +0000

    added helpful console log to illustrate load order stuff a little better

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 // the params passed into the function are the corresponding objects which require loads in
 // require(["dependency1", "dependency2"],function(dependency1, dependency2){
  require(["dependency1", "dependency2"],function(dependency1_but_this_can_be_called_anything_really, dependency2){
+	 console.log("this won't be executed until the full dependency chain has been loaded");
 	 console.log(dependency1_but_this_can_be_called_anything_really.my_method());
 	 console.log(dependency2);
 });
