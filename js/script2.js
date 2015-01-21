7394de273f196c5d83d9c57881f87ae06b8ee861
Dominic McAdden <dominic.mcadden@gmail.com>
2015-01-21 16:16:32 +0000

    added some other sample scripts to check out how the async loading works - logging useful stuff out to the console

---
+++ js/script2.js
@@ -0,0 +1,1 @@
+console.log("script 2 executed second");
---
+++ js/some_other_script.js
@@ -0,0 +1,1 @@
+console.log("since require specified that its tag is async this could be executed before the require script - don't assume any of our require content will be present!");
